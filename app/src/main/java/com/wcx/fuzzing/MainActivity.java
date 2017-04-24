d400f0af7c7f9f22634b170a17064c7a319a75dd
wcx <369808898@qq.com>
2017-04-24 13:37:21 +0800

    initial commit

---
+++ app/src/main/java/com/wcx/fuzzing/MainActivity.java
@@ -0,0 +1,17 @@
+package com.wcx.fuzzing;
+
+import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
